0ae4798da060d35d25e14da9b5803243a5a18a6b
Lucien <lulu.senaneuch@gmail.com>
2018-02-09 15:59:31 +0100

    projet init

---
+++ sources/src/components/Footerbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-footer class="pa-3" color="pink darken-3">
+        <div style="color:white;">L.Senaneuch</div>
+        <v-spacer></v-spacer>
+        <a id="buglink" href="mailto:senaneuc@insa-toulouse.fr?subject=BugDumby&body=Merci de décrire le bug observé et l'occurence de celui-ci.">Signaler un bug</a> 
+        <v-spacer></v-spacer>
+        <img class="logo-vue" src="static/logo.png" alt="logo-vue">
+        <img class="logo-vue" src="static/logo-node.png" alt="logo-node">
+        <v-spacer></v-spacer>
+        <div style="color:white;">© INSA - Toulouse {{ new Date().getFullYear() }}</div>
+    </v-footer>
+</template>
+
+<style>
+    .logo-vue{
+        width: 30px;
+        margin-left:15px;
+    }
+    #buglink{
+        color:white;
+    }
+</style>
---
+++ sources/src/components/GokuStepper.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <img id="goku" :src="gokuSource" alt="Goku Stepper">
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+      return {
+          sayajin: 0,
+      }
+  },
+  computed:{
+      gokuSource:function(){
+          return `/static/Goku_${this.sayajin}.png`
+      }
+  }
+}
+</script>
+
+
+<style>
+    #goku{
+        height: 70px;
+    }
+</style>
